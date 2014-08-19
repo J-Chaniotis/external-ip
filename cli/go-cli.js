2e2a9bdad48b6410b375c8f4e84f98dd997a2732
tazgr <taz1991@gmail.com>
2014-08-19 18:37:23 +0300

    Minor changes to output. Code cleanup.

--- cli/go-cli.js
+++ cli/go-cli.js
@@ -8,7 +8,7 @@ var GetOpt = require('node-getopt');
 //CLI parser Object
 var getOpt = new GetOpt([
 	['h',	'help'			,	'display this help'],
-	['r',	'replace'		,	'set to replace services with -s insted of adding'],
+	['r',	'replace'       ,	'set to replace services with -s insted of adding'],
 	['s',	'services=ARG+'	,	'add service, one per -s (if not set uses default list)'],
 	['t',	'timeout=ARG'	,	'set timeout per request (default 500ms)'],
 	['P',	'parallel'		,	'set to parallel mode (default sequential)']
@@ -23,44 +23,28 @@ var getOpt = new GetOpt([
 )
 .parseSystem();
 
-//Vars
-var parallelFlag = false;
-var replaceFlag = false;
-var services = null;
-var timeout = 500;
+//Config generator
+var generateConfig = function (cliConf) {
+    var config = {};
+    config.getIP = cliConf.parallel ? 'parallel' : 'sequential';
+    if (cliConf.timeout)
+        config.timeout = parseInt(cliConf.timeout);
+    if (cliConf.replace)
+        config.replace = cliConf.replace;
+    if(cliConf.services)
+        config.services = cliConf.services;
+    return config;
+};
+//Function that handles the output
+var ipPrint = function(err,ip){
+    if (err)
+        console.log("Could not retrieve IP. Check internet connection.");
+    else
+        console.log(ip);
+};
 
-var config = {};
+//Passes the config
+var getIP = extIP( generateConfig(getOpt.options) );
 
-//Set values from parser
-if(getOpt.options.parallel)
-	parallelFlag = true;	
-if(getOpt.options.replace)
-	replaceFlag = true;
-if(getOpt.options.services){
-	services = getOpt.options.services+"";
-	services = services.split(',');
-	services.toString();
-}if(getOpt.options.timeout)
-	timeout = getOpt.options.timeout;
-
-//Fill the config
-if(Object.keys(getOpt.options).length!=0){
-	config.timeout = timeout;
-	config.getIP = 'sequential';
-}
-
-//Adjust config
-if(parallelFlag==true)
-	config.getIP = 'parallel';
-if(services != null)
-	config.services = services;
-
-
-var getIP = extIP(config);
-
-getIP(function (err, ip) {
-    if (err) {
-        throw err;
-    }
-    console.log(ip);
-});
+//Runs the main thing
+getIP(ipPrint);
