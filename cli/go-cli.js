b4b9da8c61ef780ace6d78ffacc69671b25699c4
tazgr <taz1991@gmail.com>
2014-08-18 19:31:30 -0700

    Fixed cli parser module.

--- cli/go-cli.js
+++ cli/go-cli.js
@@ -3,29 +3,38 @@
 
 //Dependencies
 var getIP = require('../index')();
-var getOpt = require('node-getopt');
+var GetOpt = require('node-getopt');
 
-//Main function
-var ipPrint = function(err,ip) {
-    if (err)
-    	console.log("Failed");
- 	else
-    	console.log(ip);
-};
+//CLI parser Object
+var getOpt = new GetOpt([
+	['h',	'help'			,	'display this help'],
+	['r',	'replace'	,	'set to replace services with -s insted of adding'],
+	['s',	'services=ARG+'	,	'add service, one per -s (if not set uses default list)'],
+	['t',	'timeout=ARG'	,	'set timeout per request (default 500ms)'],
+	['P',	'parallel'		,	'set to parallel mode (default sequential)']
+])
+.bindHelp()
+.setHelp(
+	"\nThis program prints the external IP of the machine.\n"+
+	"Usage: go-cli [OPTION] or [OPTION] <arguement>.\n\n"+
+	"If without options, runs with default configuration.\n\n"+
+	"[[OPTIONS]]"+
+	"\n\n Git: https://github.com/J-Chaniotis/external-ip"
+)
+.parseSystem();
 
 //Vars
-var args = process.argv.length;
-var argv = process.argv;
+var parallelFlag = false;
+var replaceFlag = false;
+var services = null;
+var timeout = null;
 
-// if(args <= 2){
-// 	getIP(ipPrint);
-// }else{
-// 	console.log("not yet! :( ");
-// }
-
-for(var ar in argv){
-	switch(argv[ar]){
-		case 's'
-	}
-
-}
+if(getOpt.options.parallel)
+	parallelFlag = true;
+if(getOpt.options.replace)
+	replaceFlag = true;
+if(getOpt.options.services)
+	services = getOpt.options.services;
+if(getOpt.options.timeout)
+	timeout = getOpt.options.timeout;
+console.log(parallelFlag+'\t'+replaceFlag+'\t'+services+'\t'+timeout);
