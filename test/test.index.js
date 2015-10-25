27a3be858fafd686a4934bcbe3938d97aca78156
John Chaniotis <jch@geotagaeroview.com>
2015-10-26 00:34:11 +0200

    Update dependencies. Add node 4.2.1 to travis

--- test/test.index.js
+++ test/test.index.js
@@ -7,10 +7,12 @@ var extIP = require('../index');
 var utils = require('../lib/utils');
 var expect = require('chai').expect;
 
+var timeout = 3000;
+
 
 describe('index.js test', function () {
     it('Should return an IP with default configuration', function (done) {
-        this.timeout(3000);
+        this.timeout(timeout);
         var getIP = extIP();
         getIP(function (err, ip) {
             expect(err).to.equal(null);
@@ -20,12 +22,12 @@ describe('index.js test', function () {
     });
 
     it('Should return an IP with custom configuration', function (done) {
-        this.timeout(3000);
+        this.timeout(timeout);
 
         var getIP = extIP({
             replace: true, // true: replace the default services list, false: extend it, default: false
             services: ['http://ifconfig.co/x-real-ip', 'http://ifconfig.io/ip'],
-            timeout: 600, // set timeout per request, default: 500ms,
+            timeout: timeout, // set timeout per request, default: 500ms,
             getIP: 'parallel'
         });
 
