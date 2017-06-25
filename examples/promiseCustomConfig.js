3ab4336c5bb97223d054f1f6454b1c68c0e534b6
John Chaniotis <jch@geotagaeroview.com>
2017-06-25 03:42:42 +0300

    update example with promises

--- examples/promiseCustomConfig.js
+++ examples/promiseCustomConfig.js
@@ -1,17 +1,15 @@
 'use strict';
-
-const extIP = require('../index');
-
-const getIP = extIP({
+const { promisify } = require('util');
+const getIP = promisify(require('../index')({
     replace: true, // true: replace the default services list, false: extend it, default: false
     services: ['http://icanhazip.com/', 'http://ident.me/'],
     timeout: 600, // set timeout per request, default: 500ms
-    getIP: 'parallel'
-});
+    getIP: 'parallel',
+    verbose: true
+}));
 
-getIP((err, ip) => {
-    if (err) {
-        throw err;
-    }
+getIP().then((ip) => {
     console.log(ip);
+}).catch((error) => {
+    console.error(error);
 });
